c743e80e7705c2b5a97d744ac70cf4baf14d8fb9
Uttam Sojitra <uttamsojitra635@gmail.com>
2023-05-31 18:34:00 +0530

    refresh token

--- Demo/Controllers/LoginController.cs
+++ Demo/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Demo.Business.Interface.Interface_Service;
+﻿using Demo.Business.Exception;
+using Demo.Business.Interface.Interface_Service;
 using Demo.Entities.Model;
 using Demo.Repository.Interface;
 using Microsoft.AspNetCore.Authorization;
@@ -34,36 +35,80 @@ namespace Demo.Controllers
             return user;   
         }
 
-        private string GenerateRefreshToken()
-        {
-            var randomNumber = new byte[32];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(randomNumber);
-                return Convert.ToBase64String(randomNumber);
-            }
-        }
-       
-        private string GenerateToken(string Email)
+        private (string, string) GenerateTokens(string Email)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             var claims = new[]
             {
-                new Claim("Email",Email),
-                
-            };
-            var token = new JwtSecurityToken(
+               new Claim("Email", Email)
+             };
+
+            var accessToken = new JwtSecurityToken(
                 _config["Jwt:Issuer"],
                 _config["Jwt:Audience"],
                 claims,
-                expires: DateTime.Now.AddMinutes(10),
+                expires: DateTime.Now.AddMinutes(5), // Set the access token expiration time
                 signingCredentials: credentials);
-       
-            return new JwtSecurityTokenHandler().WriteToken(token);
-            //Uses an instance of JwtSecurityTokenHandler to write the JWT token as a string.
-        }   
-        
+
+            var refreshToken = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(10), // Set the refresh token expiration time
+                signingCredentials: credentials);
+
+            var accessTokenString = new JwtSecurityTokenHandler().WriteToken(accessToken);
+            var refreshTokenString = new JwtSecurityTokenHandler().WriteToken(refreshToken);
+
+            return (accessTokenString, refreshTokenString);
+        }
+
+
+        private ClaimsPrincipal ValidateRefreshToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])),
+                ValidateIssuer = true,
+                ValidIssuer = _config["Jwt:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = _config["Jwt:Audience"],
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            SecurityToken securityToken;
+            ClaimsPrincipal principal = null;
+
+            try
+            {
+                var result = tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+                if (result.Identity is ClaimsIdentity claimsIdentity)
+                {
+                    principal = new ClaimsPrincipal(claimsIdentity);
+                }
+            }
+            catch (SecurityTokenException)
+            {
+                // Handle token validation error
+                // Return an appropriate response or throw a custom exception
+                // You can customize the error message or error handling based on your requirements
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions if necessary
+                // Return an appropriate response or throw a custom exception
+                // You can customize the error message or error handling based on your requirements
+                return null;
+            }
+
+            return principal;
+        }
+
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login(string Email, string Password)
@@ -72,11 +117,24 @@ namespace Demo.Controllers
             var validUser = await AuthnticateUser(Email, Password);
             if (validUser != null)
             {
-                var accessToken = GenerateToken(Email);
-                var refreshToken = GenerateRefreshToken();
-                response =  Ok(new {token = accessToken, refreshToken = refreshToken });                 
+                var (accessToken, refreshToken) = GenerateTokens(Email);
+                response = Ok(new { token = accessToken, refreshToken = refreshToken });
             }
-            return response;        
+            return response;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refreshTokenValidation")]
+        public IActionResult RefreshToken(string refreshToken)
+        {
+            var principal = ValidateRefreshToken(refreshToken);
+            if (principal != null)
+            {
+                var email = principal.Claims.First(c => c.Type == "Email").Value;
+                var (accessToken, newRefreshToken) = GenerateTokens(email);
+                return Ok(new { token = accessToken, refreshToken = newRefreshToken });
+            }
+            return Unauthorized();
         }
     }
 }
